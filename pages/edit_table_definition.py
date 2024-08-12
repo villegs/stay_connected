7a9068b8cf4a1bfd6f5b2b9487619b9d9f60571d
pablo_v <villegs@gmail.com>
2024-08-12 02:12:14 -0400

    update for v2 of stay connected

--- pages/edit_table_definition.py
+++ pages/edit_table_definition.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 st.title("Edit report table definition")
 df = pd.read_csv('report_table_definition.csv')
+
 # Display dataframe
 st.dataframe(df)
-
 
 col1, col2 = st.columns(2)
 with col1:
@@ -15,16 +15,19 @@ with col1:
     new_example = st.text_input("Example:")
     if st.button("Add Row"):
         new_row = pd.DataFrame({'Column': [new_col], 'Type': [new_type], 'Example':[new_example]})
-        df = pd.concat([df, new_row], ignore_index=True)
-        st.dataframe(df)
+        df_updated = pd.concat([df, new_row], ignore_index=True)
+        st.dataframe(df_updated)
+        if st.button("Save changes"):
+            df_updated = df_updated.set_index('Column')
+            df_updated.to_csv('report_table_definition.csv')
 
 with col2:
     # Delete row button
     row_index = st.number_input("Enter row index to delete:", min_value=0, max_value=len(df)-1)
     if st.button("Delete Row"):
-        df = df.drop(index=row_index)
-        st.dataframe(df)
+        df_updated = df.drop(index=row_index)
+        st.dataframe(df_updated)
+        if st.button("Save changes"):
+            df_updated = df_updated.set_index('Column')
+            df_updated.to_csv('report_table_definition.csv')
 
-
-if st.button("Save changes"):
-    df.to_csv('report_table_definition.csv')
--- pages/process_pending_reports.py
+++ pages/process_pending_reports.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 df_reports = pd.read_csv('center_reports.csv', parse_dates=['Timestamp'], dayfirst=True).set_index(['Center','Week'])
 df_reports_table = pd.read_csv('reports_table.csv', parse_dates=['Timestamp'], dayfirst=True).set_index(['Center','Week'])
-
+df_report_definition = pd.read_csv('report_table_definition.csv')
 df_filtered = df_reports[~df_reports.index.isin(df_reports_table.index)]
 st.subheader('Pending reports to be converted:')
 # Display filtered DataFrame
@@ -16,14 +16,14 @@ df_choice = st.radio("Select what reports to process:", ('pending', 'all'))
 if st.button("Process"):
     if df_choice == 'pending':
         with st.spinner('Processing pending entries'):
-            processed_df = transform_reports_to_table(df_filtered.reset_index())
+            processed_df = transform_reports_to_table(df_filtered.reset_index(),df_report_definition)
             st.write("Processed pending reports:")
             st.dataframe(processed_df)
             processed_df = pd.concat([df_reports_table, processed_df])
             processed_df.to_csv('reports_table.csv')
     elif df_choice == 'all':
         with st.spinner('Processing all entries'):
-            processed_df = transform_reports_to_table(df_reports)
+            processed_df = transform_reports_to_table(df_reports,df_report_definition)
             st.write("Processed all reports:")
             st.dataframe(processed_df)
             processed_df.to_csv('reports_table.csv')

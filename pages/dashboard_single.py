5721e63ddb97493a24e96bbaf84f771c7fc851a4
pablo_v <villegs@gmail.com>
2024-08-12 00:11:38 -0400

    update for v2 of stay connected

--- pages/dashboard_single.py
+++ pages/dashboard_single.py
@@ -2,21 +2,44 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 from utils.load_and_join_df import retrieve_formatted_df
-df = retrieve_formatted_df()
-
+df_all = retrieve_formatted_df()
+center_selection = st.selectbox("Select Center", df_all['Center'].unique())
+df = df_all[df_all['Center']==center_selection].reset_index()
 # Streamlit App
 st.title("Hospital Performance Dashboard")
 
+
+
+# Filter data based on the selected center
+serve_time_cols = [
+                    "Average Time to Get Served (Urgent)",
+                    "Average Time to Get Served (Semi-Urgent)",
+                    "Average Time to Get Served (Non-Urgent)",
+                ]
+df_urgency = df[['Center', 'Week',*serve_time_cols]].copy()
+df_urgency = pd.melt(df_urgency,id_vars=[col for col in df_urgency.columns if col not in serve_time_cols],value_vars=serve_time_cols,var_name='Urgency', value_name='Average Time to Get Served')
+df_urgency['Urgency'] = df_urgency['Urgency'].str.split(' ').str.get(-1).str[1:-1]
+
+
+filtered_data = df_urgency[df_urgency['Center'] == center_selection]
 # Row 1: Center-Specific Metrics
 col1, col2 = st.columns(2)
 
 with col1:
     st.header("Center Performance")
-    st.metric("Number of Patients", df["Number of Patients"][0], delta=None)
-    st.metric("Average Time to Get Served", df["Average Time to Get Served"][0], delta=None, help="Minutes")
+    st.metric(
+        "Number of Patients",
+        round(df["Number of Patients"].mean()),
+        delta=None)
+    st.metric(
+        "Average Time to Get Served",
+        round(df["Average Time to Get Served"].mean()),
+        delta=None,
+        help="Minutes"
+    )
     st.metric(
         "Patient Satisfaction Score",
-        df["Patient Satisfaction Scores"][0],
+        round(df["Patient Satisfaction Scores"].mean()),
         delta=None,
         help="Out of 5",
     )
@@ -25,12 +48,12 @@ with col2:
     st.header("Staff Performance")
     st.metric(
         "Number of Staff Training Hours",
-        df["Number of Staff Training Hours"][0],
+        round(df["Number of Staff Training Hours"].mean()),
         delta=None,
     )
     st.metric(
         "Turnover Rate of Staff",
-        df["Turnover Rate of Staff"][0],
+        round(df["Turnover Rate of Staff"].mean()),
         delta=None,
         help="Percentage",
     )
@@ -53,33 +76,19 @@ with col2:
     st.header("Resource Utilization")
     st.metric(
         "Bed Occupancy Rate",
-        df["Bed Occupancy Rate"][0],
+        round(df["Bed Occupancy Rate"].mean()),
         delta=None,
         help="Percentage",
     )
     st.metric(
         "Utilization of Equipment & Resources",
-        df["Utilization of Equiment & Resources"][0],
+        round(df["Utilization of Equiment & Resources"].mean()),
         delta=None,
         help="Percentage",
     )
 
 # Row 3: Line Chart Column for Average Time to Get Served
 st.header("Average Time to Get Served by Urgency")
-serve_time_cols = [
-                    "Average Time to Get Served (Urgent)",
-                    "Average Time to Get Served (Semi-Urgent)",
-                    "Average Time to Get Served (Non-Urgent)",
-                ]
-df_urgency = df[['Center', 'Week',*serve_time_cols]].copy()
-df_urgency = pd.melt(df_urgency,id_vars=[col for col in df_urgency.columns if col not in serve_time_cols],value_vars=serve_time_cols,var_name='Urgency', value_name='Average Time to Get Served')
-df_urgency['Urgency'] = df_urgency['Urgency'].str.split(' ').str.get(-1).str[1:-1]
-# st.dataframe(df_urgency)
-# Create a dropdown for selecting the center
-center_selection = st.selectbox("Select Center", df_urgency['Center'].unique())
-
-# Filter data based on the selected center
-filtered_data = df_urgency[df_urgency['Center'] == center_selection]
 
 # Create Altair chart
 chart = alt.Chart(filtered_data).mark_line().encode(
@@ -93,27 +102,6 @@ chart = alt.Chart(filtered_data).mark_line().encode(
 
 # Display the chart in Streamlit
 st.altair_chart(chart, use_container_width=True)
-# chart = (
-#     alt.Chart(df_urgency)
-#     # alt.Chart(df)
-#     .mark_line()
-#     .encode(
-#         alt.X("Week:Q", title="Week"),
-#         alt.Y(
-#             "Average Time to Get Served:Q",
-#             title="Average Time (Minutes)",
-#             scale=alt.Scale(domain=[0, 60]),
-#         ),
-#         alt.Color(
-#             "Urgency",
-#             scale=alt.Scale(
-#                 domain=serve_time_cols,
-#                 range=["red", "orange", "green"],
-#             ),
-#         ),
-#     )
-# )
-# st.altair_chart(chart)
 
 # Row 4: Table with Key Metrics
 st.header("Key Metrics")
@@ -139,24 +127,24 @@ st.write(f"**Population:** {df['Population'][0]}")
 
 # Row 6: Additional Data
 st.header("Additional Data")
-st.write("**Number of Readmissions:**", df["Number of Readmissions"][0])
+st.write("**Number of Readmissions:**", round(df["Number of Readmissions"].mean()))
 st.write(
     "**Number of Medical Licenses Presented:**",
-    df["Number of Medical Licenses Presented"][0],
+    round(df["Number of Medical Licenses Presented"].mean()),
 )
 st.write(
     "**Number of  Supplies Ordered/Consumed:**",
-    df["Number of  Supplies Ordered/Consumed"][0],
+    round(df["Number of  Supplies Ordered/Consumed"].mean()),
 )
 st.write(
     "**Number of Outpatient Appointments:**",
-    df["Number of Outpatient Appointments"][0],
+    round(df["Number of Outpatient Appointments"].mean()),
 )
 st.write(
     "**Number of Emergency Room Visits:**",
-    df["Number of Emergency Room Visits"][0],
+    round(df["Number of Emergency Room Visits"].mean()),
 )
 st.write(
     "**Number of Surgical Procedures Performed:**",
-    df["Number of Surgical Procedures Performed"][0],
+    round(df["Number of Surgical Procedures Performed"].mean()),
 )
